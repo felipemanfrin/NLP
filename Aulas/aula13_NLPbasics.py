d60d35ae1f852245af437459751d410f4327f32a
felipemanfrin <felipemanfrin@gmail.com>
2020-07-02 16:22:57 -0300

    atualizaçoes pip

--- Aulas/aula13_NLPbasics.py
+++ Aulas/aula13_NLPbasics.py
@@ -1,4 +1,8 @@
 # Spacy é uma biblioteca de NLP
 # NLTK - Natural Language Toolkit (lançada em 2001, diferente da spacy que foi lançada em 2015)
 
+import PyPDF2
 import spacy
+nlp = spacy.load('en_core_web_sm')
+
+doc = nlp(u'Tesla is lookong at buying U.S startup for $6 million')

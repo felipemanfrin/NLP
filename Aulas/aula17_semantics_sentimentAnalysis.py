6cac1e745fecc2d568177aacce35a78351c86fe3
felipemanfrin <felipemanfrin@gmail.com>
2020-09-07 19:49:03 -0300

    VADER

--- Aulas/aula17_semantics_sentimentAnalysis.py
+++ Aulas/aula17_semantics_sentimentAnalysis.py
@@ -13,21 +13,52 @@ nlp = spacy.load('en_core_web_lg')
 # tokens = nlp(u'dog cat nargle')
 # for token in tokens:
 #     print(token.text, token.has_vector, token.vector_norm, token.is_oov)#out of vocabulary (oov)
-from scipy import spatial
+# from scipy import spatial
+#
+# cosine_similarity = lambda vec1, vec2: 1 - spatial.distance.cosine(vec1, vec2)
+# king = nlp.vocab['king'].vector
+# man = nlp.vocab['man'].vector
+# woman = nlp.vocab['woman'].vector
+#
+# new_vector = king-man+woman
+# computed_similarities = []
+# # PARA TODAS AS PALAVRAS NO VOCAB
+# for word in nlp.vocab:
+#     if word.has_vector:
+#         if word.is_lower:
+#             if word.is_alpha:
+#                 similarity = cosine_similarity(new_vector, word.vector)
+#                 computed_similarities.append((word, similarity))
+# computed_similarities = sorted(computed_similarities, key=lambda item: -item[1])#vai mostrar em ordem decrescente a similaridade
+# print([t[0].text for t in computed_similarities[:10]])
 
-cosine_similarity = lambda vec1, vec2: 1 - spatial.distance.cosine(vec1, vec2)
-king = nlp.vocab['king'].vector
-man = nlp.vocab['man'].vector
-woman = nlp.vocab['woman'].vector
+#-------------------------------------------------------------------------------
+#VADER ( VALENCE AWARE DICTIONARY FOR SENTIMENT REASONING) ta no pacote NLTK
 
-new_vector = king-man+woman
-computed_similarities = []
-# PARA TODAS AS PALAVRAS NO VOCAB
-for word in nlp.vocab:
-    if word.has_vector:
-        if word.is_lower:
-            if word.is_alpha:
-                similarity = cosine_similarity(new_vector, word.vector)
-                computed_similarities.append((word, similarity))
-computed_similarities = sorted(computed_similarities, key=lambda item: -item[1])#vai mostrar em ordem decrescente a similaridade
-print([t[0].text for t in computed_similarities[:10]])
+import nltk
+# nltk.download('vader_lexicon')
+from numpy import np
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+sid = SentimentIntensityAnalyzer()
+a = "This is a good movie"
+b = 'This was the best, most awesome movie EVER MADE!!@!'
+print(sid.polarity_scores(a))
+print(sid.polarity_scores(b))
+import pandas as pd
+df = pd.read_csv('textos/amazonreviews.tsv', sep='\t')
+print(df.head())
+print(df['label'].value_counts())
+df.dropna(inplace=True)
+blanks = []
+for i, lb, rv in df.itertuples():
+    if type == str:
+        if rv.isspace():
+            blanks.append()
+#df.drop(blanks, inplace=True) se precisar tirar alguma coisa faltando
+print(df.iloc[0]['review'])
+print(sid.polarity_scores(df.iloc[0]['review']))
+df['scores'] = df['review'].apply(lambda review: sid.polarity_scores(review))
+df['compound'] = df['scores'].apply(lambda d:d['compound'])
+print(df.head())
+
+
